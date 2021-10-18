21c858a5774af1756181ce3a5c2514b43442e6ae
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-10-18 10:49:16 -0300

    Sending email

--- src/main/java/github/guisofiati/email/resources/EmailResource.java
+++ src/main/java/github/guisofiati/email/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package github.guisofiati.email.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import github.guisofiati.email.dto.EmailDTO;
+import github.guisofiati.email.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
 	
+	
+	@Autowired
+	private EmailService service;
+	
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 }
